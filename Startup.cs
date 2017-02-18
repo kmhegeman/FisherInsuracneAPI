859b5d7642ebc8e6c53563dd0dd2b3b19b46070e
kmhegeman <hegeman.1@osu.edu>
2017-02-18 15:21:29 -0500

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using FisherInsuranceApi.Data;
 
 namespace FisherInsuranceApi
 {
@@ -28,6 +29,8 @@ namespace FisherInsuranceApi
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
+            services.AddSingleton<IMemoryStore, MemoryStore>();
+            
             services.AddMvc();
         }
 
